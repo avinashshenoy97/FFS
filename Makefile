cab407ebf3987274539366e60dfe9efe569bdc56
Avinash Shenoy <avi123shenoy@hotmail.com>
2018-02-12 01:15:31 +0530

    MKFS creates root

--- Makefile
+++ Makefile
@@ -1,25 +1,31 @@
 username = $(shell whoami)
-files = ffs_main.c ffs_operations.c tree.c disk.c
+files = ffs_operations.c tree.c disk.c bitmap.c
 compileflags = -D_FILE_OFFSET_BITS=64
 opflag = -o ffs
-neededflag = `pkg-config fuse --libs` -DFUSE_USE_VERSION=22
+neededflag = `pkg-config fuse --libs` -DFUSE_USE_VERSION=22 -lm
 
 all: run
 
 run: compile	
-	./ffs -f /home/$(username)/Desktop/mountpoint
+	./ffs -f /home/$(username)/Desktop/mountpoint /home/$(username)/Desktop/file.txt
 
 drun: dcompile
-	./ffs -d -f -s /home/$(username)/Desktop/mountpoint 
+	./ffs -d -f -s /home/$(username)/Desktop/mountpoint /home/$(username)/Desktop/file.txt
 
 bgrun: compile
 	./ffs /home/$(username)/Desktop/mountpoint
 
 compile: checkdir
-	gcc -Wall $(files) $(compileflags) $(opflag) $(neededflag)
+	gcc -Wall ffs_main.c $(files) $(compileflags) $(opflag) $(neededflag)
 
 dcompile: checkdir
-	gcc -Wall -g -DERR_FLAG $(files) $(compileflags) $(opflag) $(neededflag)	
+	gcc -Wall -g -DERR_FLAG ffs_main.c $(files) $(compileflags) $(opflag) $(neededflag)
 
 checkdir:
 	if [ -d "/home/$(username)/Desktop/mountpoint" ]; then echo "mountpoint exists"; else mkdir /home/$(username)/Desktop/mountpoint; fi
+
+mkfs: mkfs_compile
+	./mkfs /home/$(username)/Desktop/file.txt
+
+mkfs_compile: 
+	gcc -Wall -g -DERR_FLAG $(files) mkfs.c $(compileflags) -o mkfs $(neededflag)
--- mkfs.c
+++ mkfs.c
@@ -1,13 +1,106 @@
+/*
+	1. Create the file
+	2. Create superblock
+	3. Write size of disk as first 64 bits of file
+	4. Write size of bitmap (in blocks) required for entire disk in next 64 bits
+	5. Create root ("/") directory
+	6. Mark blocks used by superblock, bitmap and root directory as 1 in bitmap
+	7. Write bitmap to file
+	8. Write root node to file
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
 #include<fcntl.h>
 
-int main(){
-int i;
-int buf[10]={0,0,0,0,0,0,0,0,0,0};
-int fd = open("file.txt",O_CREAT|O_RDWR,0666);
-for(i=0;i<(1024*1024*1024/10);i++)
-	write(fd,(void*)buf,sizeof(buf));
-return 0;
+#include "tree.h"
+#include "disk.h"
+#include "bitmap.h"
+
+// macros for backward compatibility
+#define openDisk(x) openDisk(x, 0)
+
+int diskfd;
+
+int main(int argc, char **argv){
+	init_fs();		// Creates root directory
+
+	uint64_t i, size = 1 * 1024 * 1024;	// 1 MB
+	int fd = open(argv[1], O_CREAT | O_TRUNC | O_RDWR, 0666);
+	diskfd = fd;
+
+	uint8_t to_write = 0;
+	for(i = 0 ; i < size ; i++) {
+		write(fd, &to_write, sizeof(to_write));
+	}
+	printf("%s\n", "Done creating!");
+
+	// Write size of disk to superblock
+	lseek(fd, 0, SEEK_SET);
+	write(fd, &size, sizeof(size));
+	printf("Wrote size %llu to file\n", size);
+
+	// Calculate size of BITMAP in bits
+	uint64_t bsize = size / BLOCK_SIZE;
+	// Size of BITMAP in bytes
+	bsize /= 8;
+	// Size of BITMAP in blocks
+	bsize /= BLOCK_SIZE;
+	bsize++;
+	printf("bsize %llu to file\n", bsize);
+	bmap_size = bsize;
+
+	// Write number of blocks taken by bitmap in superblock
+	lseek(fd, sizeof(size), SEEK_SET);
+	write(fd, &bsize, sizeof(bsize));
+	printf("Wrote bsize %llu to file\n", bsize);
+
+	// Blocks needed by BITMAP, to be marked as 1 in bitmap
+	uint64_t bmap_blocks = bsize / BLOCK_SIZE;
+	bmap_blocks++;
+
+	// First (bmap_blocks) need to marked with 1 in BITMAP
+	printf("Marking first %d blocks\n", bmap_blocks + SUPERBLOCKS);
+
+	bitmap = calloc(bsize, BLOCK_SIZE);
+	if(!bitmap) {
+		perror("No memory for bitmap");
+		exit(0);
+	}
+	for(i = 0 ; i < bmap_blocks + SUPERBLOCKS ; i++)
+		setBitofMap(i);
+
+	printf("Done marking!\n");
+
+	void *buf;
+	uint64_t firstFreeBlock = findFirstFreeBlock();
+	printf("First free block = %d\n", firstFreeBlock);
+	printf("Constructing block for root node!\n");
+	
+	fs_tree_node *root = node_exists("/");
+	root->inode_no = firstFreeBlock;
+	constructBlock(root, &buf);		// Create block for root node
+
+	output_node(*root);
+	
+	printf("FIRST free block = %d\n", firstFreeBlock);
+	//memcpy(buf + (BLOCK_SIZE - 8*2), &firstFreeBlock, sizeof(firstFreeBlock)); // set the inode field
+	printf("FIRST free block = %d\n", firstFreeBlock);
+
+	printf("Done constructing block for root node!\n");
+	writeBlock(firstFreeBlock, buf);
+	printf("FIRST free block = %d\n", firstFreeBlock);
+
+	printf("Done writing block for root node!\n");
+	setBitofMap(firstFreeBlock);
+	printf("Writing bitmap to file\n");
+	for(i = 0 ; i < bmap_blocks ; i++) {
+		writeBlock(SUPERBLOCKS + i, bitmap + (i * BLOCK_SIZE));
+	}
+	
+	printf("Freeing, closing, end!\n");
+	free(buf);
+	close(fd);
+	return 0;
 }
